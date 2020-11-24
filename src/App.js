cc7db56e094f69e63e5fc2c1600f15023884d262
Hector Lima <hectoraugusto12@yahoo.ie>
2020-11-24 11:57:17 +0000

    done, by participants, byprice

--- src/App.js
+++ src/App.js
@@ -8,22 +8,30 @@ import ByPrice from "./components/byprice/ByPrice";
 import ByParticipants from "./components/byparticipants/ByParticipants";
 import ByAccesibility from "./components/byaccesibility/ByAccesibility";
 import Random from "./components/Random";
+import PriceLimit from "./components/byprice/PriceLimit";
+import ParticipantsNumber from "./components/byparticipants/ParticipantsNumber";
 
 function App() {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/type" component={ByType} />
-          <Route exact path="/price" component={ByPrice} />
-          <Route exact path="/participants" component={ByParticipants} />
-          <Route exact path="/accesibility" component={ByAccesibility} />
-          <Route exact path="/random" component={Random} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="container">
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={Homepage} />
+					<Route exact path="/type" component={ByType} />
+					<Route exact path="/price" component={ByPrice} />
+					<Route exact path="/participants" component={ByParticipants} />
+					<Route exact path="/accesibility" component={ByAccesibility} />
+					<Route exact path="/random" component={Random} />
+					<Route exact path="/price/:price" component={PriceLimit} />
+					<Route
+						exact
+						path="/participantsnumber/:participants"
+						component={ParticipantsNumber}
+					/>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
